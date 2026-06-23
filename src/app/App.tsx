826937756db7308b5a0da2377e4e09e87620f245
Vincent Morrill <vmorrilll@gmail.com>
2026-06-23 14:15:21 -0500

    fix: remove public generator editor route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,10 @@
-import { BrowserRouter, useLocation } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { AppShell } from "../components/layout/AppShell";
-import { TokenforgeIntakePage } from "../pages/TokenforgeIntakePage";
 
 export function App() {
   return (
     <BrowserRouter basename="/tokenforge-printdesk">
-      <AppContent />
+      <AppRoutes />
     </BrowserRouter>
   );
-}
-
-function AppContent() {
-  const location = useLocation();
-  if (location.pathname === "/generator") {
-    return (
-      <AppShell>
-        <TokenforgeIntakePage />
-      </AppShell>
-    );
-  }
-
-  return <AppRoutes />;
 }
